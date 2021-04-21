8f31457bd1a0eddb04a81073fe09f9ebb8ffc219
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-04-20 23:08:04 -0400

    Section 7 - Adding behaviour to nav elements

--- section-6/omnifood/resources/js/script.js
+++ section-6/omnifood/resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  /** for Sticky nav */
   $('.js--section-features').waypoints
   
   var waypoint = new Waypoint({
@@ -14,7 +15,13 @@ $(document).ready(function() {
     offset: '6.5%'
   })
   
+  /** for scroll on buttons */
+  $('.js--scroll-to-plans').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000)
+  });
 
-  
+  $('.js--scroll-to-start').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 500)
+  });
 
 })
